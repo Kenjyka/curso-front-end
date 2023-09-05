42701b6bd32a7ba90a218cef0279d592bff8b1d4
ead <ead@academico.local>
2023-09-05 17:26:38 -0300

    finalizando display minor

--- vscode/html/ex12/calculator3000.js
+++ vscode/html/ex12/calculator3000.js
@@ -1,6 +1,7 @@
 const numbers = Array.from(document.querySelectorAll('.number'))
 const operators = Array.from(document.querySelectorAll('.operacao'))
 let display = document.getElementById('display')
+let displayMinor = display.firstElementChild
 let displayText = display.lastElementChild
 let conta = []
 let index = 0
@@ -44,8 +45,22 @@ function salvarDisplay() {
     console.log(conta[index])
 }
 function operador(operador) {
-    index++
-    conta[index] = operador
-    index++
+    if(displayText.value != undefined) {
+        index++
+        conta[index] = operador
+        index++
+        displayText.value = ''
+        console.log(conta.length)
+        for( i = 0; i <= conta.length - 1; i++ ) {
+            console.log(i)
+            if(i == 0){
+                displayMinor.innerText = conta[i]
+            } 
+            else {
+                console.log('yeya')
+                displayMinor.innerText += ` ${conta[i]} `
+            } 
+        }
+    }
 
 }
